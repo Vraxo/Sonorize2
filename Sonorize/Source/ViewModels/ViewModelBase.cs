efb1b27ce68bf311bbb559b7af1687fa918483cf
Parsa <Parsa@DESKTOP-32S8QLS>
2025-05-23 20:43:52 +0330

    Removed 'no loop defined'

--- Sonorize/Source/ViewModels/ViewModelBase.cs
+++ Sonorize/Source/ViewModels/ViewModelBase.cs
@@ -10,7 +10,11 @@ public class ViewModelBase : INotifyPropertyChanged
 
     protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
     {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        if (EqualityComparer<T>.Default.Equals(field, value))
+        {
+            return false;
+        }
+
         field = value;
         OnPropertyChanged(propertyName);
         return true;
