b078f0598ed08a43b0a363201aa22b264d561f02
Parsa <Parsa@DESKTOP-32S8QLS>
2025-06-01 14:42:01 +0330

    Refactored LoopRegion

--- Sonorize/Source/Models/LoopRegion.cs
+++ Sonorize/Source/Models/LoopRegion.cs
@@ -1,36 +1,8 @@
 ﻿using System;
-using Sonorize.ViewModels;
 
 namespace Sonorize.Models;
 
-public class LoopRegion : ViewModelBase
+public sealed record LoopRegion(TimeSpan Start, TimeSpan End, string Name = "Loop")
 {
-    public string Name
-    {
-        get;
-        set
-        {
-            SetProperty(ref field, value);
-        }
-    }
-
-    public TimeSpan Start
-    {
-        get;
-        set
-        {
-            SetProperty(ref field, value);
-        }
-    }
-
-    public TimeSpan End { get; set; }
-
     public string DisplayText => $"({Start:mm\\:ss} - {End:mm\\:ss})";
-
-    public LoopRegion(TimeSpan start, TimeSpan end, string name = "Loop")
-    {
-        Name = name;
-        Start = start;
-        End = end;
-    }
 }
